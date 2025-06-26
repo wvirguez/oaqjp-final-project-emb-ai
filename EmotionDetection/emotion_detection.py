beafa5eb38a4755bb14b26ac6d73524e768caef0
wvirguez <wvirguezp@gmail.com>
2025-06-26 17:35:06 -0400

    EmotionDetector Function

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,9 +1,13 @@
 import requests  # Import the requests library to handle HTTP requests
 import json
+# Watson NLP  pre-trained models
+import watson_nlp
+import watson_nlp.data_model as dm
 
 def emotion_detector(text_to_analyze):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL of the sentiment analysis service
     myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
+    #header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     # Parsing the JSON response from the API
