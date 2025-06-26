c3eb58230a323490141a44701410fe81b10794a5
wvirguez <wvirguezp@gmail.com>
2025-06-26 16:50:10 -0400

    Fixing a Module call bug

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion__detector
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Sentiment Analyzer")
 
